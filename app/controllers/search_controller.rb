36f63957d6eb2493e14f8d05ce4bff5dae01995f
Michael DaSilva <programmedmikey@gmail.com>
2025-09-29 19:29:00 -0400

    search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,25 @@
 class SearchController < ApplicationController
   def listings
+    results = RentcastSearchService.new.rental_listings(search_params)
+    render json: results
+  rescue => e
+    render json: { error: e.message }, status: :bad_request
+  end
+
+  private
+
+  def search_params
+    params.permit(
+      :city, :state, :zipCode, :latitude, :longitude, :radius,
+      :propertyType, :bedrooms, :bathrooms, :squareFootage, :lotSize, :yearBuilt,
+      :price, :daysOld, :status, :listingType, :limit, :offset,
+      :min_bedrooms, :max_bedrooms,
+      :min_bathrooms, :max_bathrooms,
+      :min_price, :max_price,
+      :min_year_built, :max_year_built,
+      :min_square_footage, :max_square_footage,
+      :min_lot_size, :max_lot_size,
+      :min_days_old, :max_days_old
+    )
   end
 end
